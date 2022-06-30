f3ee71fc9dac22114b8ce4d3221623cba01b8f54
Juaniman <manzonijuan@gmail.com>
2022-06-30 19:20:36 -0300

    commit inicial clase 19

---
+++ AppCoder/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from AppCoder.models import Curso
+
+# Create your views here.
+
+def curso(self):
+    curso=Curso(nombre='Django', comision=96659)
+    curso.save()
+    texto=f"Curso creado: {curso.nombre} , {curso.comision}"
+    return HttpResponse(texto)
